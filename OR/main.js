1c0fa23f83d1ec1f8dfb41234f80c225361cc72f
JoseCruz2 <josefernandocruzcarrillo22@gmail.com>
2022-05-19 20:26:57 -0500

    Operadores and,or,xor

---
+++ OR/main.js
@@ -0,0 +1,11 @@
+let A = (prompt("Ingrese el valor de la variable A\n false\n true") == "true") ? true : false;
+let B = (prompt("Ingrese el valor de la variable B\n false\n true") == "true") ? true : false;
+
+
+document.querySelector("#operadorAND").insertAdjacentHTML("beforeend", `
+    <tr class="resultado">
+        <td>${A}</td>
+        <td>${B}</td>
+        <td>${A && B}</td>
+    </tr>
+`);
---
+++ XOR/main.js
@@ -0,0 +1,10 @@
+let A = (prompt("Ingrese el valor de la variable A\n false\n true") == "true") ? true : false;
+
+
+
+document.querySelector("#operadorAND").insertAdjacentHTML("beforeend", `
+    <tr class="resultado">
+        <td>${A}</td>
+        <td>${A && B}</td>
+    </tr>
+`);
